055c0cb2d001c721dc4e47fb4a080b5f3b8344bd
tyler <tyler.giczkowski@marquette.edu>
2025-10-10 15:20:31 -0500

    Added: [1] Start Screen, [2] End screen], [3] endless game loop: start ->play -> gameover -> start, ...
    Needed: [1] Score to be displayed during and after play.
    
    Bugs created: [1] when starting the game any directional input is allowed.

--- snake.py
+++ snake.py
@@ -16,23 +16,56 @@ pygame.display.set_caption("Snake Game")
 clock = pygame.time.Clock()
 
 def main():
-    cell = Cell(row, col)
     game_logic = GameLogic(row, col)
     input_handler = InputHandler()
     renderer = Renderer()
+    alive = False
     
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-                raise SystemExit
-        #main game loop    
-        input_data = input_handler.get_input()
-        game_logic.update(input_data)#do logic updates
-        renderer.render(screen, game_logic.grid, cell_size)#render graphics
-        pygame.display.flip()   #update the full display Surface to the screen
-        clock.tick(60)          #wait until next frame(60fps)
+        #start screen loop 
+        while not alive:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                    raise SystemExit
+            input_data = input_handler.get_input()
+            #game_logic.update(input_data)#do logic updates Here we can do bot logic while waiting for user to start
+            alive = game_logic.start_game(input_data, row, col)
+            renderer.render(screen, game_logic.grid, cell_size)#render graphics
+            renderer.render_start_screen(screen)
+            pygame.display.flip()   #update the full display Surface to the screen
+            clock.tick(60)          #wait until next frame(60fps)
+        
+        #main game loop   
+        while alive:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                    raise SystemExit 
+            input_data = input_handler.get_input()
+            game_logic.update(input_data)#do logic updates
+            alive = game_logic.snake_state()#check if game over
+            renderer.render(screen, game_logic.grid, cell_size)#render graphics
+            pygame.display.flip()   #update the full display Surface to the screen
+            clock.tick(60)          #wait until next frame(60fps)
+            
+        #end game screen loop 
+        while not alive:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                    raise SystemExit
+            input_data = input_handler.get_input()
+            #game_logic.update(input_data)#do logic updates Here we can do bot logic while waiting for user to start
+            alive = game_logic.start_game(input_data, row, col)
+            renderer.render(screen, game_logic.grid, cell_size)#render graphics
+            renderer.render_end_screen(screen)
+            pygame.display.flip()   #update the full display Surface to the screen
+            clock.tick(60)          #wait until next frame(60fps)
+
 
 if __name__ == "__main__":
     main()
